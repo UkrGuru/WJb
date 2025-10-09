95ca8dec1538c0605760e0efb613c44638e78dd0
A V <alexander@businessnetsolutions.co.uk>
2025-10-09 12:59:13 +0300

    2025-10

---
+++ src/Scheduler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace UkrGuru.WJb;
+
+public class Scheduler(ILogger<Scheduler> logger) : BackgroundService
+{
+    private readonly ILogger<Scheduler> _logger = logger;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Scheduler running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
---
+++ src/Worker.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace UkrGuru.WJb;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    private readonly ILogger<Worker> _logger = logger;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
